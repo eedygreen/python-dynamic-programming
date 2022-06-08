8bce2efb410168ff7c35dc7775341f587b5bd2c2
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-06-08 10:25:43 +0100

    string concatenations
    f string as a my prefer choice for string concatenations

--- lists/12_python_project/string_concatentation.py
+++ lists/12_python_project/string_concatentation.py
@@ -7,14 +7,33 @@ f string - This is prefferable because its straight forward and simple
 .format
 '''
 
+# answ = input("enter the name of your best friend: ")
 
-answ = input("enter the name of your best friend: ")
+# digit = " Dude"
 
-digit = " Dude"
+# # Example of f string
+# def stringJoiner(data):
+#     con_cat = f"{data} is the real {digit}!"
+#     return con_cat
 
-# Example of f string
-def stringJoiner(data):
-    con_cat = f"{data} is the real {digit}!"
-    return con_cat
+# print(stringJoiner(answ))
 
-print(stringJoiner(answ))
+
+'''
+Steps
+1. define an input variable to allow users to add their friend's name
+
+2. Write a function that accept the users's input and add it to predefined strings
+
+3. print the result
+'''
+
+# 1
+user_name = input("Please, enter your friend's name here: ")
+
+#2, use f string
+def joinName( data ):
+    join_names = f"Your friend, {user_name} is lucky to have you. {user_name} is a nice buddy!"
+    return join_names
+
+print(joinName(user_name))
