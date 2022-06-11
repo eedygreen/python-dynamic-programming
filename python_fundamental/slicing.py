12fc6072348b1cc9c512f4846b873340ce3808e1
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-06-11 20:55:14 +0100

    Development (#3)
    * list comprehension
    
    * merge sorted list
    
    * merge sorted list
    
    * merge sorted list
    
    * add two numbers bruteforce method
    
    * f string
    
    * string concatenations
    
    f string as a my prefer choice for string concatenations
    
    * slicing
    
    * python_fumdamental
    
    Dynamic programming with Educative

---
+++ python_fundamental/slicing.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+slice_string = "This is My String"
+
+print(slice_string[:5]) # all characters to fifth
+
+print(slice_string[6:]) # all characters from sixth all the way to end
+
+print(slice_string[:]) # all characters
+
+print(slice_string[::-1]) # all characters in reverse with 1 step
