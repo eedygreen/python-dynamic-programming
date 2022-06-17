aaffd2bead2d3b3986ac8b45350817b62c11f622
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-06-17 10:38:11 +0100

    simplify the operations

--- python_fundamental/simple_calculator.py
+++ python_fundamental/simple_calculator.py
@@ -1,4 +1,6 @@
 #!/bin/python
+def calculate(operator, n1, n2):
+    return operator(n1, n2)
 
 def add(n1, n2):
     return n1 + n2
@@ -12,14 +14,14 @@ def divide(n1, n2):
 def subtract(n1, n2):
     return n1 - n2
 
-def calculate(operator, n1, n2):
-    return operator(n1, n2)
-
 print(calculate(add, 3, 4))
 print(calculate(multiply, 3, 4))
 
 # use lambda to simplify the function
-
-add_num = calculate(lambda n1, n2: n1 + n2, 3, 4)
+# add func = lambda n1, n2: n1 + n2
+# ...
+add_num  = calculate(lambda n1, n2: n1 + n2, 3, 4)
+mult_num = calculate(lambda n1, n2: n1 * n2, 3, 4)
 
 print(add_num)
+print(mult_num)
