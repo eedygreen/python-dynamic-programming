e7a8f0006e22be4e015cd9a5ed548c7a7e350aa1
isahidris <eedygreen@outlook.com>
2022-07-05 18:02:46 +0100

    declare the two first sequence

--- python_fundamental/recursion/fibonacci_sequence.py
+++ python_fundamental/recursion/fibonacci_sequence.py
@@ -11,4 +11,36 @@ def fibo(n):
     else: 
         return fibo(n - 1) + fibo( n - 2)
 
-print(fibo(3))
+print(fibo(6))
+
+# define a function
+# declare three variables with two constant,
+# first sequence is zero
+# second sequence is 1
+# third variable is to add the two frist variable 
+# compare the given number and return the sequence
+# repeate
+
+def fib(n):
+    first = 0
+    second = 1
+
+    if n < 1:
+        return -1
+
+    if n == 1:
+      return first
+
+    if n == 2:
+        return second
+
+    counter = 3
+
+    while counter <= n:
+        fib_seq = first + second
+        first = second
+        second = fib_seq
+        counter +=1
+    return fib_seq
+
+print(fib(6))
