03bbb4c7c7e5b97726ebe1330ac4b96f47710661
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-11-01 15:22:59 +0100

    second solution: replace elements with Greatest elements on the right side

--- freekcode/replace_elements.py
+++ freekcode/replace_elements.py
@@ -1,6 +1,7 @@
 #Given an array arr, replace every element in that array with the greatest element among the elements to its right,
 #  and replace the last element with -1.
 
+# solution 1, best case
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
         # iniitialize max
@@ -16,4 +17,16 @@ class Solution:
             if tmp > max_arr:
                 max_arr = tmp   # swap max; update the max value
             k -= 1
+        return arr
+
+# solution 2 with for loops
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        last_ele = -1
+
+        for i in range(len(arr)-1, -1, -1):
+            tmp = arr[i]
+            arr[i] = last_ele
+            if tmp > last_ele:
+                last_ele = tmp
         return arr
